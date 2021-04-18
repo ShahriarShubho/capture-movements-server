cc36a3c740f3bc01d68ea97197d2a528e8d156d7
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-04-18 14:11:31 +0600

    create admin api

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ client.connect((err) => {
     .db(`${process.env.DB_NAME}`)
     .collection("review");
 
+    const bookingCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection("booking");
+
+    const adminCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection("admin");
+
 
   app.post('/addServices', (req, res) => {
     const service = req.body
@@ -41,19 +49,58 @@ client.connect((err) => {
 
 app.post('/addReview', (req, res) => {
   const review = req.body
-  console.log(review)
   reviewCollection.insertOne(review)
   .then(result => {
-    console.log(result.insertedCount, "inserted")
     res.send(result.insertedCount > 0)
   })
 })
+
+app.post('/addBooking', (req, res) => {
+  const booking = req.body
+  bookingCollection.insertOne(booking)
+  .then(result => {
+    res.send(result.insertedCount > 0)
+  })
+})
+
+app.post('/makeAdmin', (req, res) => {
+  const admin = req.body
+  console.log(admin);
+  adminCollection.insertOne(admin)
+  .then(result => {
+    res.send(result.insertedCount > 0)
+  })
+})
+
+app.post('/isAdmin', (req, res) => {
+  const email = req.body.email
+  adminCollection.find({email: email})
+  .toArray((err, admin) => {
+    res.send(admin.length > 0)
+  })
+  
+})
+
 
 app.get('/bookingById/:id', (req, res) => {
   serviceCollection.find({_id: ObjectId(req.params.id)})
   .toArray((err, book) => {
     res.send(book[0])
   })
+})
+
+
+app.patch('/update/:id', (req, res) => {
+  console.log(req.body.status)
+  bookingCollection.updateOne({_id: ObjectId(req.params.id)},
+  {
+    $set : {status : req.body.status}
+  })
+  .then(result =>{
+    res.send(result.modifiedCount > 0)
+    console.log(result)
+  })
+
 })
 
   app.get("/services", (req, res) => {
@@ -69,6 +116,29 @@ app.get('/bookingById/:id', (req, res) => {
       res.send(review);
     });
   });
+
+  app.get("/orders", (req, res) => {
+    bookingCollection.find({})
+    .toArray((err, orders) => {
+      res.send(orders);
+    });
+  });
+
+  app.get("/booking", (req, res) => {
+    bookingCollection.find({email : req.query.email})
+    .toArray((err, booking) => {
+      res.send(booking);
+    });
+  });
+
+  app.delete('/delete/:id', (req, res) => {
+    console.log(req.params.id)
+    serviceCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then(result =>{
+      console.log(result)
+      res.send(result.deletedCount > 0)
+    })
+  })
 
 });
 
